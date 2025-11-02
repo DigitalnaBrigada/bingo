1042fc0f3a5a0a26f92bd7bd5c59314743c262f0
zigaz <ziga.zadravec@student.um.si>
2025-11-02 15:38:13 +0100

    Redo preload.js and renderer.js.

--- preload.js
+++ preload.js
@@ -1,13 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-// supabase
-const {createClient} = require("@supabase/supabase-js");
-import { Database } from './supabase'
-const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping')
-})
+contextBridge.exposeInMainWorld('api', {
+    getAgeGroups: async () => {
+        return ipcRenderer.invoke('get-age-groups');
+    },
+});
