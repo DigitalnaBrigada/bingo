e8070a8b645939f85fe41a31ead056e84bb35db0
Nik Coh <nik.coh@student.um.si>
2025-11-03 14:39:25 +0100

    Fixed incorrect parameters in the exposed leaderboard api.

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('api', {
     loadMenu: () => ipcRenderer.invoke('loadMenu'),
-    leaderboard: (group) => ipcRenderer.invoke('leaderboard', { group }),
+    leaderboard: (groups, categories) => ipcRenderer.invoke('leaderboard', { groups, categories }),
     startGame: (group, categories) => ipcRenderer.invoke('startGame', { group, categories }),
     answer: (questionId, selectedIndex) => ipcRenderer.invoke('answer', { questionId, selectedIndex }),
 });
