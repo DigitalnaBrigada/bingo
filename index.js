1bc81083d684493e9d7aeeb8222b420d6492bb2e
Nik Coh <nik.coh@student.um.si>
2025-11-03 14:54:30 +0100

    Added a few ; for consistency.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const {app, BrowserWindow, ipcMain} = require('electron/main')
-const path = require('node:path')
+const {app, BrowserWindow, ipcMain} = require('electron/main');
+const path = require('node:path');
 
 // supabase
 const {createClient} = require("@supabase/supabase-js");
@@ -15,8 +15,8 @@ const createWindow = () => {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
-    win.loadFile('index.html')
+    });
+    win.loadFile('index.html');
 }
 
 function createAddPlayerWindow() {
@@ -41,13 +41,13 @@ function createAddPlayerWindow() {
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle('ping', () => 'pong')
-    createWindow()
+    ipcMain.handle('ping', () => 'pong');
+    createWindow();
 
     // macOS
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+            createWindow();
         }
     })
 })
